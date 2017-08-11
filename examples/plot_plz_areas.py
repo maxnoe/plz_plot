738dcdad6a9443d5f2f7e43b65e3b0cb53d86b37
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-08-11 17:52:26 +0200

    Update example

--- examples/plot_plz_areas.py
+++ examples/plot_plz_areas.py
@@ -12,17 +12,18 @@ ax = fig.add_subplot(1, 1, 1, projection=crs.Mercator())
 
 
 df['inhabitants_per_area'] = df['inhabitants'] / df['area']
-df = df.query('inhabitants > 0 and area > 0')
 
 plot = plot_plz_data(
     df['inhabitants_per_area'],
     ax=ax,
     norm=LogNorm(),
+    vmin=1e1,
+    vmax=2.5e4,
 )
 
 ax.set_extent([4, 16, 47, 56], crs.PlateCarree())
 
-ax.set_title('Inhabitants per Square Kilometer')
+ax.set_title('Inhabitants per Square Kilometert')
 
 fig.colorbar(plot, ax=ax)
 fig.tight_layout()
