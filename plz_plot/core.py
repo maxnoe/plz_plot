616156b82fb7683acc57bef45ef19b3c39d526ae
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-08-11 17:48:11 +0200

    Use patches

--- plz_plot/core.py
+++ plz_plot/core.py
@@ -1,10 +1,24 @@
 import matplotlib.pyplot as plt
-from matplotlib.colors import Normalize
-from matplotlib.cm import ScalarMappable
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
 from cartopy import crs
 import numpy as np
 
 from .io import load_plz_records
+
+
+def build_patch_collection(recs):
+    patches = []
+    repeat = np.zeros(len(recs), dtype=int)
+    for i, rec in enumerate(recs):
+        for geom in rec.geometry:
+            repeat[i] += 1
+            try:
+                patches.append(Polygon(np.array(geom.boundary.xy).T, closed=True))
+            except NotImplementedError as e:
+                patches.append(Polygon(np.array(geom.boundary[0].xy).T, closed=True))
+
+    return PatchCollection(patches, transform=crs.Mercator()), repeat
 
 
 def plot_plz_data(
@@ -18,31 +32,18 @@ def plot_plz_data(
         ):
 
     recs = load_plz_records()
+    col, repeat = build_patch_collection(recs)
+    data = np.repeat(data, repeat)
 
     if ax is None:
         ax = plt.axes(projection=projection or crs.Mercator())
         ax.set_extent([4, 16, 47, 56], crs.PlateCarree())
 
-    norm = norm or Normalize()
-    norm.vmin = vmin or data.min()
-    norm.vmax = vmax or data.max()
-    cmap = plt.get_cmap(cmap)
+    col.set_array(data)
+    col.set_cmap(cmap)
+    col.set_norm(norm)
+    col.set_clim(vmin, vmax)
 
-    mappable = ScalarMappable(cmap=cmap, norm=norm)
-    mappable.set_array(data.values)
+    ax.add_collection(col)
 
-    for rec in recs:
-
-        try:
-            val = data.loc[rec.attributes['plz']]
-        except KeyError:
-            val = np.nan
-
-        ax.add_geometries(
-            rec.geometry,
-            crs=crs.Mercator(),
-            linewidth=0,
-            color=cmap(norm(val)),
-        )
-
-    return mappable
+    return col
