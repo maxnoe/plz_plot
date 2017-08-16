e3c1723d2d54942aa4458dc77191e22a1126a6e6
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-08-16 21:24:19 +0200

    Update example

--- examples/plot_plz_areas.py
+++ examples/plot_plz_areas.py
@@ -2,6 +2,7 @@ from plz_plot.core import plot_plz_data
 from plz_plot.io import get_plz_dataframe
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
+from cartopy import feature
 
 from cartopy import crs
 
@@ -9,6 +10,21 @@ df = get_plz_dataframe()
 
 fig = plt.figure(figsize=(8, 8))
 ax = fig.add_subplot(1, 1, 1, projection=crs.Mercator())
+
+ax.set_extent([4, 16, 47, 56], crs.PlateCarree())
+
+land = feature.NaturalEarthFeature(
+    'physical', 'land', '10m',
+    facecolor=feature.COLORS['land'],
+)
+countries = feature.NaturalEarthFeature(
+    'cultural', 'admin0_countries', '10m',
+    facecolor='none',
+    edgecolor='k',
+)
+
+ax.add_feature(land)
+ax.add_feature(countries)
 
 
 df['inhabitants_per_area'] = df['inhabitants'] / df['area']
@@ -18,10 +34,10 @@ plot = plot_plz_data(
     ax=ax,
     norm=LogNorm(),
     vmin=1e1,
-    vmax=2.5e4,
+    vmax=10e4,
 )
-
-ax.set_extent([4, 16, 47, 56], crs.PlateCarree())
+plot.set_edgecolor('lightgray')
+plot.set_linewidth(0.2)
 
 ax.set_title('Inhabitants per Square Kilometer')
 
